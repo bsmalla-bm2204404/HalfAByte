917830fc0ec62945771c6dc56fa38b4a8fe1d92b
bsmalla-bm2204404 <bsmalla.hisham@gmail.com>
2024-12-15 14:57:22 +0300

    removing

--- yalaPay/android/app/src/main/kotlin/com/example/yala_pay/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.yala_pay
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- yalaPay/lib/models/enums/country.dart
+++
@@ -1,9 +0,0 @@
-import 'package:floor/floor.dart';
-
-@Entity(tableName: "countries")
-class Country {
-  @PrimaryKey()
-  final String countryName;
-
-  Country({required this.countryName});
-}
